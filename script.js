cd4da6b452158f1edb6db7f2ca96583b02e804fe
Anjali Goyal <anjali24.7goyal@gmail.com>
2021-05-28 20:31:32 +0530

    Calculator completed: HTML, CSS, JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // getting all input values
+    var principal = document.getElementById("principal");
+    var amount = principal.value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = amount * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    // Invalid principal amount
+    if(amount <= 0){
+        alert("Enter a positive number");
+
+        // focus in the principal input again
+        principal.focus();
+    } else {
+        // if inputs are valid, set innerHTML of result
+        document.getElementById("result").innerHTML="If you deposit <mark>"+amount+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    }
 }
-        
+
+function updateRate() 
+{
+    // getting rate value
+    var rateval = document.getElementById("rate").value;
+
+    // changing the value on the webpage
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
